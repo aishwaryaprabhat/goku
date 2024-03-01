14f30875eb7373b97012399cafd7e7bde63a7c04
Aish <aishwaryaprabhat@gmail.com>
2024-03-01 06:10:26 +0000

    beginning work for model ingestion

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # GOKU: <u>G</u>enAI<u>O</u>ps on <u>Ku</u>bernetes
-## A reference architecture for performing Generative AI Operations (aka GenAIOps) using Kubernetes-native open source tools
+A reference architecture for performing Generative AI Operations (aka GenAIOps) using Kubernetes, with open source tools
 
 ![](docs/assets/goku.webp)
 
--- scripts/useful_commands.sh
+++ scripts/useful_commands.sh
@@ -2,7 +2,7 @@ kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.pas
 kubectl -n argowf exec -it $(kubectl get pods -n argowf | grep argo-workflows-server | awk '{print $1}') -- argo auth token
 
 # Kill prior services running on the ports
-for port in 9090 8080 9093 8501 5000 2746; do
+for port in 8080 9001 9000 5000 2746; do
   fuser -k $port/tcp
 done
 
@@ -10,8 +10,8 @@ kubectl -n argocd port-forward svc/argocd-server 8080:80 &
 kubectl -n minio port-forward svc/minio-console 9001 &
 kubectl -n minio port-forward svc/minio 9000 &
 kubectl -n mlflow port-forward svc/mlflow 5000 &
-kubectl -n postgresql port-forward svc/postgres-postgresql 5432 &
 kubectl -n argowf port-forward svc/argowf-argo-workflows-server 2746 &
+kubectl -n postgresql port-forward svc/postgres-postgresql 5432 &
 
 # psql -h localhost -p 5432 -U postgresw
 # kubectl -n mlflow delete pods --all --grace-period=0 --force
