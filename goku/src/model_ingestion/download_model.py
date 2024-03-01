334f9529ebe626db589abfa61d9fbd601e833af9
Aish <aishwaryaprabhat@gmail.com>
2024-03-01 09:23:39 +0000

    attempting preflight checks

--- goku/src/model_ingestion/download_model.py
+++ goku/src/model_ingestion/download_model.py
@@ -2,12 +2,11 @@ import os
 import argparse
 from transformers import AutoModel, AutoTokenizer
 
-def download_model(repo=None, model_name=None):
+def download_model(model_name="Mistral-7B-Instruct-v0.2"):
     # Use environment variables as defaults if no arguments are provided
-    if repo is None:
-        repo = os.getenv("HUGGINGFACE_REPO", "mistralai/Mixtral-8x7B-Instruct-v0.1")
+
     if model_name is None:
-        model_name = os.getenv("MODEL_NAME", "Mixtral-8x7B-Instruct-v0.1")
+        model_name = os.getenv("HF_MODEL_NAME", model_name)
 
     # Downloading and saving the model and tokenizer
     model = AutoModel.from_pretrained(repo)
@@ -22,12 +21,10 @@ if __name__ == "__main__":
     # Set up argument parser
     parser = argparse.ArgumentParser(description='Download and save a model from Hugging Face.')
 
-    # Adding optional arguments
-    parser.add_argument('--repo', type=str, help='The Hugging Face repository to download the model from.')
     parser.add_argument('--model', type=str, help='The name under which to save the model and tokenizer.')
 
     # Parse the command line arguments
     args = parser.parse_args()
 
     # Call download_model with CLI arguments or None (to use defaults)
-    download_model(args.repo, args.model)
+    download_model(args.model)
